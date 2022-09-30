a3e1a5e3a2eea4ac2c26d686645be301fa804fd3
Pingvu <nhlpool27@gmail.com>
2022-09-30 14:29:52 +0200

    Add project files.

---
+++ uppgift 3.18/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Uppgift_3_18
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in förnamn och efternamn");
+            string x=Console.ReadLine();
+            int mellanslagsindex = x.IndexOf(" ");
+           
+            string förnamn=x[..mellanslagsindex];
+            string efternamn = x [(mellanslagsindex + 1)..];
+            if (förnamn.CompareTo(efternamn) < 0)
+            {
+                Console.WriteLine(förnamn + "kommer före " + efternamn + " i bokstavsordning");
+            }
+            else
+            {
+                Console.WriteLine(efternamn + " kommer före " + förnamn + " i bokstavsordning ");
+            }
+
+        }
+    }
+}
